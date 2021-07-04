77559b02d847e000a7b3ec005bd0ebebab7d181b
xezzz <pschaper18@gmail.com>
2021-07-04 20:05:57 +0200

    ✨ Added stuff for PyPi

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="sentinel-python",
+    version="0.0.1",
+    author="xezzz",
+    description="Package for using Discord slash commands",
+    packages=find_packages(),
+    install_requires=["websocket-client", "aiohttp"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
+)
